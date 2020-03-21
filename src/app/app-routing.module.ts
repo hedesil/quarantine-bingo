2f5a203ab3c899ff6ad57d88868e5cd7609980be
Mariano Moreno <mariano25294@gmail.com>
2020-03-21 11:26:11 +0100

    Initial commit of Angular 9 project with 2 components and Angular Material.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainViewComponent} from './views/main-view/main-view.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainViewComponent, pathMatch: 'full'},
+  {path: 'main', component: MainViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './views/main-view/main-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import { GameMenuComponent } from './views/main-view/game-menu/game-menu.component';
+import { PlayerMenuComponent } from './views/main-view/player-menu/player-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    GameMenuComponent,
+    PlayerMenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
